cb0e1873405283eab34108ceed7e352d78a0ba03
Shreyas Bhadani <shreyas02005@gmail.com>
2026-04-14 16:22:03 +0530

    Fixed Deployment issue

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig(({ mode }) => {
       hmr: process.env.DISABLE_HMR !== 'true',
     },
     build: {
-      outDir: 'dist',
+      outDir: path.resolve(__dirname, 'dist'),
       emptyOutDir: true,
     }
   };
